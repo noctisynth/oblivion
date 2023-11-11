947f691ef6f589e455688a637ffce2f255bb3493
苏向夜 <fu050409@163.com>
2023-11-11 21:07:02 +0800

    :bug: 修复api.get参数传递的问题

--- oblivion/api.py
+++ oblivion/api.py
@@ -16,7 +16,7 @@ def request(
 
 
 def get(olps, key_pair=None, verify=True, tfo=True):
-    return request("get", olps, None, key_pair, verify, tfo)
+    return request("get", olps, None, None, key_pair, verify, tfo)
 
 
 def post(olps, data=None, file=None, key_pair=None, verify=True, tfo=True):
